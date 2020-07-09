e3330bd8be0bd9f5fa137ea713cb952c42134ca6
Paul McCarthy <pauldmccarthy@gmail.com>
2020-07-09 12:59:21 +0100

    MNT: Version 0.34.0. Install pyobjc bindings on macos. Don't sym-link FSLeyes.app. Update version/copyright in info.plist

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,4 +9,10 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   cp $RECIPE_DIR/fsleyes.icns     $PREFIX/bin/FSLeyes.app/Contents/Resources/
   cp $RECIPE_DIR/fsleyes_doc.icns $PREFIX/bin/FSLeyes.app/Contents/Resources/
   cp $PREFIX/bin/fsleyes          $PREFIX/bin/FSLeyes.app/Contents/MacOS/
+
+  PKG_COPYRIGHT=$(cat ./COPYRIGHT)
+  PLIST=$PREFIX/bin/FSLeyes.app/Contents/Info.plist
+
+  sed -ie "s/##VERSION##/$PKG_VERSION/g"     $PLIST
+  sed -ie "s/##COPYRIGHT##/$PKG_COPYRIGHT/g" $PLIST
 fi
--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,7 +1,3 @@
 if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonLink.sh ]; then
   $FSLDIR/etc/fslconf/requestFSLpythonLink.sh fsleyes render
-
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    $FSLDIR/etc/fslconf/requestFSLpythonLink.sh FSLeyes.app
-  fi
 fi
