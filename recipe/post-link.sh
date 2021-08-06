f6cdad668145c1af92eff1526a8379caa7e8bd46
Paul McCarthy <pauldmccarthy@gmail.com>
2021-08-06 14:01:28 +0100

    MNT: support new FSL wrapper link scripts

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,5 +1,6 @@
+# FSL 6.0.5 and older
 if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonLink.sh ]; then
-  $FSLDIR/etc/fslconf/requestFSLpythonLink.sh fsleyes render
+  ${FSLDIR}/etc/fslconf/requestFSLpythonLink.sh fsleyes render
 
   # Hack for FSL 6.0.5:
   #
@@ -8,7 +9,10 @@ if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonLink.sh ]; then
   # "python" rather than "pythonw", which
   # causes GUI apps to fail on macOS.
   if [[ "$OSTYPE" == "darwin"* ]] && [[ "$PREFIX" == "$FSLDIR"* ]]; then
-    sed -i "" 's/python -I/pythonw -I/g' $FSLDIR/bin/fsleyes
-    sed -i "" 's/python -I/pythonw -I/g' $FSLDIR/bin/render
+    sed -i "" 's/python -I/pythonw -I/g' ${FSLDIR}/bin/fsleyes
+    sed -i "" 's/python -I/pythonw -I/g' ${FSLDIR}/bin/render
   fi
+# FSL 6.0.6 and newer
+elif  [ -e ${FSLDIR}/share/fsl/sbin/createFSLWrapper ]; then
+  ${FSLDIR}/share/fsl/sbin/createFSLWrapper fsleyes render
 fi
--- recipe/pre-unlink.sh
+++ recipe/pre-unlink.sh
@@ -1,7 +1,9 @@
 if [ -e ${FSLDIR}/etc/fslconf/requestFSLpythonUnlink.sh ]; then
-  $FSLDIR/etc/fslconf/requestFSLpythonUnlink.sh fsleyes render
+  ${FSLDIR}/etc/fslconf/requestFSLpythonUnlink.sh fsleyes render
 
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    $FSLDIR/etc/fslconf/requestFSLpythonUnlink.sh FSLeyes.app
+    ${FSLDIR}/etc/fslconf/requestFSLpythonUnlink.sh FSLeyes.app
   fi
+elif  [ -e ${FSLDIR}/share/fsl/sbin/removeFSLWrapper ]; then
+  ${FSLDIR}/share/fsl/sbin/removeFSLWrapper fsleyes render
 fi
